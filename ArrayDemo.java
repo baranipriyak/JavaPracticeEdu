cf70fac23dbb230440747e4a9b497382a51b273b
baranipriyak <141120736+baranipriyak@users.noreply.github.com>
2024-11-13 15:20:44 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,21 @@
+package firstpack;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declare array
+		
+        //int a[]=new int[5];
+        //or
+       // int b[];
+      //  b=new int[5];
+        
+        int c[]= {5,3,4,6,8};
+                //0 1 2 3 4
+        for(int i=0;i<c.length;i++)
+        System.out.println("c["+i+"]="+c[i]);
+
+	}
+
+}
